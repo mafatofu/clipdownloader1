7905b1a147b9b817d3a2f514be8c021c98642c49
“mafatofu” <qwqw0310@gmail.com>
2024-08-19 12:41:22 +0900

    시작

---
+++ src/main/java/com/example/clipdownloader1/Clipdownloader1Application.java
@@ -0,0 +1,13 @@
+package com.example.clipdownloader1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clipdownloader1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Clipdownloader1Application.class, args);
+    }
+
+}
