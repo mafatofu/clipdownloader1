6631bb28e35be7a9437fe70df98810dd1d404a0d
“mafatofu” <qwqw0310@gmail.com>
2024-08-26 23:11:03 +0900

    dto 추가

---
+++ src/main/java/com/example/clipdownloader1/dto/MainDto.java
@@ -0,0 +1,14 @@
+package com.example.clipdownloader1.dto;
+
+import lombok.*;
+
+//dto
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MainDto {
+    private Long id;
+    private String name;
+}
