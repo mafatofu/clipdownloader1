3436e81e8b430876c9ac0f521fdcff3875cbed1d
“mafatofu” <qwqw0310@gmail.com>
2024-11-20 17:00:42 +0900

    fix : github actions 테스트2

--- src/main/java/com/example/clipdownloader1/repo/SportRedisRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.clipdownloader1.repo;
-
-import com.example.clipdownloader1.entity.Sport;
-import org.springframework.data.repository.CrudRepository;
-
-/**redis test용 repo*/
-public interface SportRedisRepo extends CrudRepository<Sport, Long> {
-
-}
