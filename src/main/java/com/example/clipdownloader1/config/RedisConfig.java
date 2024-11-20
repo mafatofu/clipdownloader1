7c139c1fa865160ef0478cec161ab67a120460db
“mafatofu” <qwqw0310@gmail.com>
2024-11-20 14:48:54 +0900

    fix : redis bean error fix

--- src/main/java/com/example/clipdownloader1/config/RedisConfig.java
+++ src/main/java/com/example/clipdownloader1/config/RedisConfig.java
@@ -10,15 +10,11 @@ import org.springframework.data.redis.core.RedisTemplate;
 /**redis config. Lettuce 사용*/
 @Configuration
 public class RedisConfig {
-    @Value("${Spring.data.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String host;
-    @Value("${Spring.data.redis.port}")
+    @Value("${spring.data.redis.port}")
     private int port;
 
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
-    }
     @Bean
     LettuceConnectionFactory connectionFactory(){
         return new LettuceConnectionFactory(host, port);
